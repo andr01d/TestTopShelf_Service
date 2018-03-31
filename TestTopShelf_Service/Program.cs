13b55c75d6f6b749cd3d20e43192b0687d5e9042
Andrei Lozada Villegas <andrei.lozada@outlook.com>
2018-03-31 03:11:42 -0600

    Carga inicial

---
+++ TestTopShelf_Service/Program.cs
@@ -0,0 +1,87 @@
+﻿using FluentScheduler;
+using System;
+using Topshelf;
+
+namespace TestTopShelf_Service
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x => {
+                x.Service<IWindowsService>(s =>
+                {
+                    s.ConstructUsing(name => new WindowsService(new SchedulerRegistry(new Worker())));
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Test");
+                x.SetDisplayName("Test Service");
+                x.SetServiceName("Testservice");
+
+                x.StartAutomatically();
+
+                x.EnableServiceRecovery(s =>
+                {
+                    s.RestartService(1);
+                    s.RestartService(2);
+                });
+            });
+        }
+    }
+
+    public class SchedulerRegistry : Registry
+    {
+        public SchedulerRegistry(Worker worker)
+        {
+            Schedule(() =>
+            {
+                try
+                {
+                    worker.Run();
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }).NonReentrant().ToRunNow().AndEvery(10).Seconds();
+        }
+    }
+
+    public interface IWindowsService
+    {
+        void Start();
+        void Stop();
+    }
+
+    public class WindowsService : IWindowsService
+    {
+        public WindowsService(SchedulerRegistry registry)
+        {
+            JobManager.Initialize(registry);
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Service started");
+        }
+
+        public void Stop()
+        {
+            JobManager.StopAndBlock();
+            Console.WriteLine("Service stopped");
+        }
+    }
+
+    public class Worker
+    {
+        public void Run()
+        {
+            Console.WriteLine("Running job...");
+        }
+    }
+}
